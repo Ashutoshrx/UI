7f84506c24d2f0e3ea8e21584548ce88f5829db2
Ashutoshrx <asutosh007satpathy@gmail.com>
2023-05-18 17:46:09 +0530

    exercise-1 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Enact UI';
-  name=''
+  disableAlert: boolean = true;
+  showSuccessAlert: boolean = false;
+  showErrorAlert: boolean = false;
+  constructor() {
+    setTimeout(() => {
+      // console.log("Buttons Enabled");    
+      this.disableAlert = false;
+    }, 3000);
+  }
+  showSuccessMsg(){
+    // console.log("Success message clicked");
+    this.showSuccessAlert = true;    
+  }
+  showErrorMsg(){
+    this.showErrorAlert = true;
+    // console.log("Warning message clicked");
+}
 }
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  // styleUrls: ['./warning-alert.component.css']
+  styles:[`
+  p{
+    color: white;
+    padding:10px;
+    background-color: red;
+  }`]
+})
+export class WarningAlertComponent {
+
+}
